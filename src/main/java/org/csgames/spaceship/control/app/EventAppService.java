6b0e9be9da8359aedce694d58b3b2860bb70e311
Marshall Asch <maasch@rogers.com>
2018-03-24 10:56:06 -0400

    This is not working for F3 still, change it code does not effect acceptacne test results

--- src/main/java/org/csgames/spaceship/control/app/EventAppService.java
+++ src/main/java/org/csgames/spaceship/control/app/EventAppService.java
@@ -1,5 +1,6 @@
 package org.csgames.spaceship.control.app;
 
+import lombok.NonNull;
 import org.csgames.spaceship.sdk.SpaceshipSdk;
 import org.csgames.spaceship.sdk.service.AwayTeamLogService;
 import org.csgames.spaceship.sdk.service.PlanetRegistry;
@@ -14,8 +15,9 @@ public class EventAppService {
     this.sdk = sdk;
   }
 
+  private HashMap<String, String> telemetry = new HashMap<>();
+
   public void handleReceivedEvent(EventDto eventDto) {
-    // FIXME: Implement core logic to handle received events
 
     int remainingFish = 0;
     int remainingWater = 0;
@@ -25,23 +27,41 @@ public class EventAppService {
 
       case "DATA_MEASURED":
 
+        String dataKey = genTelemetryKey(eventDto);
+
+        if (telemetry.containsKey(dataKey) && telemetry.get(dataKey).equals(eventDto.payload)) {
+          // the data is already there do nothing
+        }
+        else if (telemetry.containsKey(dataKey) && !telemetry.get(dataKey).equals(eventDto.payload)) {
+          // the data is already there the value needs to be updated
+          sdk.getAwayTeamLogService().reportMeasureData(eventDto.source, eventDto.payload);
+          telemetry.replace(dataKey, eventDto.payload);
+        }
+        else {
+          sdk.getAwayTeamLogService().reportMeasureData(eventDto.source, eventDto.payload);
+          telemetry.put(dataKey, eventDto.payload);
+        }
+
         // save the data
-        awayTeamLogService.reportMeasureData(eventDto.source, eventDto.payload);
+        sdk.getAwayTeamLogService().reportMeasureData(eventDto.source, eventDto.payload);
         break;
       case "SUPPLY_CONSUMED":
 
         String[] parts = eventDto.payload.split(",");
 
-        if (parts == null || parts.length == 0) {
+        // make sure that the parts has values
+        if (parts.length != 2) {
           parts = new String[]{"0", "0"};
         }
 
         remainingFish = Integer.parseInt(parts[0]);
         remainingWater = Integer.parseInt(parts[1]);
 
+        // send food if needed
         if (remainingFish < 5)
           sdk.getSpaceshipService().sendFishTo(eventDto.source, 15);
 
+        // send water if needed
         if (remainingWater < 2)
           sdk.getSpaceshipService().sendWaterTo(eventDto.source, 10);
 
@@ -49,13 +69,30 @@ public class EventAppService {
 
       case "OUT_OF_SUPPLIES":
 
+        // send supplies to starving penguins.
         sdk.getSpaceshipService().sendFishTo(eventDto.source, 20);
-
         sdk.getSpaceshipService().sendWaterTo(eventDto.source, 15);
 
         break;
       default:
         // none
     }
+  }
+
+
+  // this will generate the key for the hashmap for storing telemtry
+  private @NonNull String genTelemetryKey(EventDto event) {
+
+    String type = event.type;
+    String team = event.source;
+    String[] data =  event.payload.split("=");
+
+    String key = data.length == 0 ? "" : data[0];
+
+
+    String hashKey = type + team + key;
+
+    return hashKey;
+
   }
 }
