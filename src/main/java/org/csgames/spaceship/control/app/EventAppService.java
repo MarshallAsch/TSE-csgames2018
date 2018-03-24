5a5dc6ed781f8c568f508cf5292c5450c7751b26
Brandon1234 <brandonlit@gmail.com>
2018-03-24 10:24:52 -0400

    F4 Completed Requires Testing

--- src/main/java/org/csgames/spaceship/control/app/EventAppService.java
+++ src/main/java/org/csgames/spaceship/control/app/EventAppService.java
@@ -4,11 +4,30 @@ import org.csgames.spaceship.sdk.SpaceshipSdk;
 
 public class EventAppService {
 
+  private SpaceshipSdk sdk;
+
   public EventAppService(SpaceshipSdk sdk) {
     // FIXME: Use the sdk to retrieve the available services we made for you
+
+    this.sdk = sdk;
+
   }
 
   public void handleReceivedEvent(EventDto eventDto) {
     // FIXME: Implement core logic to handle received events
+
+    switch(eventDto.type){
+
+      case "OUT_OF_SUPPLIES":
+
+        sdk.getSpaceshipService().sendFishTo(eventDto.source, 20);
+
+        sdk.getSpaceshipService().sendWaterTo(eventDto.source, 15);
+
+        break;
+
+
+    }
+
   }
 }
--- src/test/java/org/csgames/spaceship/control/app/EventAppServiceTest.java
+++ src/test/java/org/csgames/spaceship/control/app/EventAppServiceTest.java
@@ -1,11 +1,25 @@
 package org.csgames.spaceship.control.app;
 
+import org.csgames.spaceship.sdk.SpaceshipSdk;
 import org.junit.Test;
 
 public class EventAppServiceTest {
 
+  private SpaceshipSdk sdk;
+
   @Test
   public void test() {
     // FIXME: Test the event app service
+
+    //F4 test data
+    EventDto event = new EventDto("OUT_OF_SUPPLIES", "away-team-01", "");
+
+
+    EventAppService testF4 = new EventAppService(sdk);
+
+
+
+
+
   }
 }
